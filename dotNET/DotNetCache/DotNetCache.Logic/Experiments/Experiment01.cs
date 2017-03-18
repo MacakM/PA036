c4d0c2de84e690e49cab57cd644d0cc5782a821a
xgasior <o.gasior@hotmail.com>
2017-03-18 18:47:01 +0100

    server log

--- dotNET/DotNetCache/DotNetCache.Logic/Experiments/Experiment01.cs
+++ dotNET/DotNetCache/DotNetCache.Logic/Experiments/Experiment01.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using DotNetCache.DataAccess.DemoDataContext;
 using EFSecondLevelCache;
 
@@ -6,6 +8,13 @@ namespace DotNetCache.Logic.Experiments
 {
     public class Experiment01 : ExperimentBase
     {
+
+        private string _serverLogQuery = @"SELECT TOP 1 t.[text], s.last_execution_time
+FROM sys.dm_exec_cached_plans AS p
+INNER JOIN sys.dm_exec_query_stats AS s
+   ON p.plan_handle = s.plan_handle
+CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t
+ORDER BY s.last_execution_time DESC;";
         public Experiment01(string connectionString) : base(connectionString)
         {
         }
@@ -18,6 +27,15 @@ namespace DotNetCache.Logic.Experiments
                 db.Database.Log = s => Log += s;
                 var customers = db.Customers.Cacheable().ToList();
                 customerId = customers.First().C_CUSTKEY;
+
+                using (SqlConnection conn = new SqlConnection(db.Database.Connection.ConnectionString+ "password=DBTestPass123;"))
+                {
+                    using (SqlCommand command = new SqlCommand(_serverLogQuery, conn))
+                    {
+                        conn.Open();
+                        string result = (string)command.ExecuteScalar();
+                    }
+                }
             }
 
             using (var db = new DemoDataDbContext(ConnectionString))
