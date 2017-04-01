8209162b57b68213b71ec9b739182b6c6f2267a8
xgasior <o.gasior@hotmail.com>
2017-04-01 22:29:13 +0200

    Cache Config

--- dotNET/DotNetCache/DotNetCache.DataAccess/Cache/CacheConfiguration.cs
+++ dotNET/DotNetCache/DotNetCache.DataAccess/Cache/CacheConfiguration.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Core.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EFCache;
 
 namespace DotNetCache.DataAccess.Cache
 {
     public class CacheConfiguration : DbConfiguration
     {
+        public CacheConfiguration()
+        {
+            var transactionHandler = new CacheTransactionHandler(new InMemoryCache());
 
+            AddInterceptor(transactionHandler);
+
+            Loaded +=
+                (sender, args) => args.ReplaceService<DbProviderServices>(
+                    (s, _) => new CachingProviderServices(s, transactionHandler));
+        }
     }
 }
