a4f6b7f296484b1e5e3be47db5d805fef5550fd7
MacakM <410452@mail.muni.cz>
2017-04-07 21:12:49 +0200

    Added support for different DB sizes

--- dotNET/DotNetCache/DotNetCache.Console/Program.cs
+++ dotNET/DotNetCache/DotNetCache.Console/Program.cs
@@ -8,19 +8,28 @@ namespace DotNetCache.Console
 {
     public class Program
     {
-        public static string ConnectionString =
+        public static string ConnectionString200 =
             @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "200M.mdf") + ";Integrated Security=True";
+        public static string ConnectionString400 =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "400M.mdf") + ";Integrated Security=True";
+        public static string ConnectionString800 =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "800M.mdf") + ";Integrated Security=True";
+        public static string ConnectionString1000 =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "1000M.mdf") + ";Integrated Security=True";
+        public static string ConnectionString2000 =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "2000M.mdf") + ";Integrated Security=True";
+
         public static void Main(string[] args)
         {
 
             var experiments = new List<List<ExperimentResult>>
             {
-                new ExperimentService(new Experiment01(ConnectionString)).Start(),
-                new ExperimentService(new Experiment02(ConnectionString)).Start(),
-                new ExperimentService(new Experiment03(ConnectionString)).Start(),
-                new ExperimentService(new Experiment04(ConnectionString)).Start(),
-                new ExperimentService(new Experiment05(ConnectionString)).Start(),
-                new ExperimentService(new Experiment06(ConnectionString)).Start(),
+                new ExperimentService(new Experiment01(ConnectionString200)).Start(),
+                new ExperimentService(new Experiment02(ConnectionString200)).Start(),
+                new ExperimentService(new Experiment03(ConnectionString200)).Start(),
+                new ExperimentService(new Experiment04(ConnectionString200)).Start(),
+                new ExperimentService(new Experiment05(ConnectionString200)).Start(),
+                new ExperimentService(new Experiment06(ConnectionString200)).Start(),
             };
 
             for (int i = 0; i < experiments.Count; i++)
