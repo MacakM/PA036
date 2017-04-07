8ab2c16da5912c100b03a40406fe1abb202125fc
xgasior <o.gasior@hotmail.com>
2017-04-07 12:57:48 +0200

    dhdffh

---
+++ dotNET/DotNetCache/DotNetCache.Api/Controllers/BobTheBuilderController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web.Http;
+using DotNetCache.Api.Models;
+using DotNetCache.Logic.Experiments;
+using DotNetCache.Logic.Services;
+
+namespace DotNetCache.Api.Controllers
+{
+    public class BobTheBuilderController : ApiController
+    {
+        public ExperimentService ExperimentService;
+        public static string ConnectionString =
+            @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "200M.mdf") + ";Integrated Security=True";
+
+        [HttpGet]
+        [Route("api/BobTheBuilder/Experiment/{id}")]
+        public string /*ExperimentInfo*/ Experiment(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    ExperimentService = new ExperimentService(new Experiment01(ConnectionString));
+                    break;
+                case 2:
+                    ExperimentService = new ExperimentService(new Experiment02(ConnectionString));
+                    break;
+                case 3:
+                    ExperimentService = new ExperimentService(new Experiment03(ConnectionString));
+                    break;
+                case 4:
+                    ExperimentService = new ExperimentService(new Experiment04(ConnectionString));
+                    break;
+                case 5:
+                    ExperimentService = new ExperimentService(new Experiment05(ConnectionString));
+                    break;
+                default:
+                    throw new ArgumentException("Experiment does not exist or is not registered");
+            }
+            var result = ExperimentService.Start();
+            return result.Select(x => x.ToString()).Aggregate((st, nd) => $"{st}\n{nd}");
+        }
+    }
+}
---
+++ dotNET/DotNetCache/DotNetCache.Api/Models/ExperimentInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using DotNetCache.Logic.Experiments;
+
+namespace DotNetCache.Api.Models
+{
+    public class ExperimentInfo
+    {
+        public ExperimentBase Experiment { get; set; }
+        public List<ExperimentResult> Results { get; set; }
+    }
+}
