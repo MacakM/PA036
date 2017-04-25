38efcd822c03208cfcea112d1654f1a7311f2b7f
xgasior <o.gasior@hotmail.com>
2017-04-25 22:39:57 +0200

    Experiment 16

--- dotNET/DotNetCache/DotNetCache.Logic/Experiments/Experiment16.cs
+++ dotNET/DotNetCache/DotNetCache.Logic/Experiments/Experiment16.cs
@@ -1,29 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading;
+using DotNetCache.DataAccess.DemoDataContext;
+using DotNetCache.DataAccess.DemoDataEntities;
+using EFCache;
+using EFSecondLevelCache;
 
 namespace DotNetCache.Logic.Experiments
 {
     /**
      * Experiment has to prove cache inconsistency when db is updated from multiple sources
-     * 1) PHP instance calls experiment 16
-     * 2) .NET instance calls experiment 16
-     * 3) PHP instance calls experiment 16 2nd time - changes the value
-     * 4) .NET instance calls experiment 16 2nd time - gets invalid value 
+     * 1) .NET instance calls experiment 16, makes query - waits 10 second
+     * 2) PHP instance calls experiment 16 - changes something within this 10 seconds
+     * 3) .NET makes same query  - query shall hit the cache even if it is not valid
      */
     public class Experiment16 : ExperimentBase
     {
+        private List<Customer> _cached;
         public Experiment16(string connectionString) : base(connectionString)
         {
         }
 
         public override List<ExperimentResult> Start()
         {
-            throw new NotImplementedException();
+            using (var db = new DemoDataDbContext(ConnectionString))
+            {
+                Console.WriteLine("Getting data to cache");
+                var customers = db.Customers.Cacheable().Where(c => c.C_CUSTKEY < 750).ToList();
+                _cached = db.Customers.Cacheable().Where(c => c.C_CUSTKEY < 750).ToList();
+                Console.WriteLine("Sleeping");
+                Thread.Sleep(10000);
+                Console.WriteLine("Woke up");
+                _cached = db.Customers.Cacheable().Where(c => c.C_CUSTKEY < 750).ToList();
+                Console.WriteLine("If true, and PHP done its job, data are inconsistent. Cached: " + InMemoryCache.LastCached); // If true, and PHP done its job, data are inconsistent.
+            }
+            return Results;
         }
 
         public override ExperimentSettings GetSettings()
         {
-            throw new NotImplementedException();
+            return new ExperimentSettings();
         }
     }
 }
