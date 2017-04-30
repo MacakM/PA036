a1e074a8ad7974b333764c3edd4e7dde271f0865
Aleš Kopecký <kopecky@flowhills.com>
2017-04-30 18:37:32 +0200

    experiments second set base

---
+++ dotNET/DotNetCache/DotNetCache.Logic/Experiments02/Experiment01.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using DotNetCache.Logic.Experiments;
+using DotNetCache.DataAccess.DemoDataContext;
+using EFSecondLevelCache;
+using System.Threading;
+
+namespace DotNetCache.Logic.Experiments02
+{
+    public class Experiment01 : ExperimentBase
+    {
+        System.Timers.Timer _timer;
+        public Experiment01(string connectionString,ExperimentSettings settings) : base(connectionString, settings)
+        {
+            _timer = new System.Timers.Timer(100);
+        }
+
+        protected override void Start()
+        {
+            using (var db = new DemoDataDbContext(ConnectionString))
+            {
+                this._timer.Elapsed += (s, e) =>
+                {
+                    this.AddResult(DemoDataDbContext.Cache.Count);
+                };
+                this._timer.Start();
+
+                Thread.Sleep(2000);
+                return;
+                //SELECT c.C_CUSTKEY FROM CUSTOMER c JOIN ORDERS o ON c.C_CUSTKEY = o.O_CUSTKEY 
+                //JOIN LINEITEM l ON l.L_ORDERKEY = o.O_ORDERKEY JOIN PART p ON l.L_PARTKEY = p.P_PARTKEY GROUP BY c.C_CUSTKEY
+                var res = db.Customers.Cacheable()
+                    .Join(db.Orders.Cacheable(), cust => cust.C_CUSTKEY, ord => ord.O_CUSTKEY, (cust, ord) => ord)
+                    .Join(db.LineItems.Cacheable(), ord => ord.O_ORDERKEY, l => l.L_ORDERKEY, (ord, l) => l)
+                    .Join(db.Parts.Cacheable(), l => l.L_PARTKEY, p => p.P_PARTKEY, (l, p) => p)
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ dotNET/DotNetCache/DotNetCache.Logic/Experiments02/ExperimentBase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using DotNetCache.DataAccess.DemoDataContext;
+using EFCache;
+using System.Diagnostics;
+using DotNetCache.Logic.Experiments;
+
+namespace DotNetCache.Logic.Experiments02
+{
+    public abstract class ExperimentBase
+    {
+        private DateTime _lastQuery;
+        protected string ConnectionString;
+        private List<ExperimentResult> Results;
+        PerformanceCounter cpuCounter;
+        ExperimentSettings _settings;
+
+        protected ExperimentBase(string connectionString,ExperimentSettings ExperimentSettings)
+        {
+            ConnectionString = connectionString;
+            _settings = ExperimentSettings;
+            Results = new List<ExperimentResult>();
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+        public string Log { get; protected set; }
+        public List<ExperimentResult> StartExperiment()
+        {
+            this.Start();
+            return this.Results;
+        }
+        protected abstract void Start();
+
+        protected void AddResult(int CachedItemsCount)
+        {
+            this.Results.Add(new ExperimentResult(DateTime.Now, GetCacheSize(), CachedItemsCount, cpuCounter.NextValue()));
+        }
+
+        protected bool DbQueryCached()
+        {
+            return InMemoryCache.LastCached;
+        }
+
+        protected double GetCacheSize()
+        {
+            long size = 0;
+            using (Stream s = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(s, DemoDataDbContext.Cache);
+                size = s.Length;
+            }
+            return (size / 1024f) / 1024f;
+        }
+
+        public virtual void PrepareSettings()
+        {
+            Experiments.ExperimentSettings.SetUpCache(_settings);
+        }
+    }
+}
